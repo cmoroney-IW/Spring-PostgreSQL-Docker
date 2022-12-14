ac74755f71ff18ed74fc612233596eb1cb9cc7be
campbell.moroney@integration.works <campbellmoroney@gmail.com>
2022-12-15 12:19:29 +1300

    Added repo functionality

---
+++ src/main/java/com/postgresspring/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.postgresspring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
--- src/main/java/com/postgresspring/PostgresSpringApplication.java
+++ src/main/java/com/postgresspring/PostgresSpringApplication.java
@@ -1,13 +1,34 @@
 package com.postgresspring;
 
-import org.springframework.boot.SpringApplication;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class PostgresSpringApplication {
+    @Autowired
+    private CustomerRepository repository; 
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostgresSpringApplication.class, args);
-	}
-
+	Logger logger
+            = Logger.getLogger(
+                PostgresSpringApplication.class.getName());
+  
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        List<Customer> allCustomers = this.repository.findAll(); 
+        logger.info("Number of customers: " + allCustomers.size());
+ 
+        Customer newCustomer = new Customer(); 
+        newCustomer.setFirstName("John"); 
+        newCustomer.setLastName("Doe"); 
+        logger.info("Saving new customer..."); 
+        this.repository.save(newCustomer); 
+ 
+        allCustomers = this.repository.findAll(); 
+        logger.info("Number of customers: " + allCustomers.size());
+    }
 }
