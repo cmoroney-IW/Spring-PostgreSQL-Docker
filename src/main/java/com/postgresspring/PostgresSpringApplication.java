733f80e61ea119e4e514db190773ee4cef74651f
campbell.moroney@integration.works <campbellmoroney@gmail.com>
2022-12-15 12:14:01 +1300

    Ran Spring Initializr

---
+++ src/main/java/com/postgresspring/PostgresSpringApplication.java
@@ -0,0 +1,13 @@
+package com.postgresspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/postgresspring/PostgresSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
